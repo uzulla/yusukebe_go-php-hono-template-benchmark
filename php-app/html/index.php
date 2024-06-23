2c0b9251a13da0f46e4c0b98c2551a3c0eaa6d60
uzulla <uzulla@himitsukichi.com>
2024-06-23 16:20:54 +0900

    php: add some cheat.

--- php-app/html/index.php
+++ php-app/html/index.php
@@ -3,6 +3,12 @@
 require_once('/app/vendor/smarty/smarty/libs/Smarty.class.php');
 $smarty = new Smarty\Smarty();
 $smarty->setCompileDir('/tmp');
+
+$smarty->caching = Smarty\Smarty::CACHING_LIFETIME_SAVED;
+$smarty->cache_lifetime = 3600; // 1 hour
+$smarty->compile_check = false;
+$smarty->force_compile = false;
+
 $data = [];
 for ($i = 0; $i < 1000; $i++) {
     $data[] = "ほげ";
